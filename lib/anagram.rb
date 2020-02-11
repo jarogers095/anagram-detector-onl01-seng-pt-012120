accee97915edaa32811ad79ef0c83cfb07b252b6
James Rogers <jarogers095@gmail.com>
2020-02-11 03:49:05 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,12 +1,17 @@
 # Your code goes here!
 class Anagram
+  attr_accessor(:word)
   
-  def initialize(word)
-    @word = word
+  def initialize(new_word)
+    @word = new_word
   end
   
-  def match(anagrams)
-    return []
+  def match(possible_anagrams)
+    original_split_sort = @word.split("").sort
+    confirmed_anagrams = possible_anagrams.collect do |anagram|
+      original_split_sort == anagram.split("").sort
+    end
+    return confirmed_anagrams
   end
   
 end
